fef25e40d0e1a5083998a3efbca4ceeaa07e2e4f
AnupamaAbin <62181521+AnupamaAbin@users.noreply.github.com>
2021-06-11 11:38:08 +0800

    Updated function for mark as completed

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,6 +9,8 @@ export default class Home extends Component {
     constructor(props) {
         super(props);
         this.editToDo = this.editToDo.bind(this);
+        this.handleRowClick = this.handleRowClick.bind(this);
+        
         this.state = {
             todos: [],
             filteredTodos: [],
@@ -23,7 +25,8 @@ export default class Home extends Component {
             
     }
 }
-    
+
+    // fetch data from url
     componentDidMount() {
         const apiUrl = 'https://jsonplaceholder.typicode.com/todos';
         fetch(apiUrl)
@@ -44,7 +47,8 @@ export default class Home extends Component {
           }
         )
       }
-
+    
+    // function: delete record from the list
     deleteToDo = (todo) => {
         const filteredItems = this.state.todos.filter(x => x.id !== todo.id);
         this.setState({
@@ -52,7 +56,7 @@ export default class Home extends Component {
         },()=>{this.updateList()});
     };
 
-
+    // function : edit todos 
     editToDo = (e,todo) => {
         e.preventDefault();
         this.setState(state => ({ editMode: !state.editMode }));
@@ -62,31 +66,33 @@ export default class Home extends Component {
         })
        this.updateList();
     };
-
+    
+    // function : pagination
     handlePageClick =(eventData)=>{
         this.setState({offset:eventData.selected},()=>{
            this.updateList();
         });
        }
-
-       manageTask = (childData) => {
+    // Add and update todos 
+    manageTask = (childData) => {
         if(this.state.editMode){
             this.setState({todos: this.state.todos.map((task) => {
                 if (task.id === childData.id) {
-                  return { ...task, title: childData.title,completed:childData.completed };
+                    return { ...task, title: childData.title,completed:childData.completed };
                 } else {
-                  return task;
+                    return task;
                 }
-              }),
+                }),
             },()=>{this.updateList()});
         }
         else{
             this.setState({todos: [...this.state.todos,childData]
             },()=>{this.updateList()});
         }
-         
-
+        
     }
+
+    // function : Categorize items  
     filterList = (type) => {
 
         if ("all"===type){
@@ -104,22 +110,41 @@ export default class Home extends Component {
         
       };
 
-      updateList=()=>{
-          if(this.state.seletedType==="all"){
-            this.setState({filteredTodos:this.state.todos.slice((this.state.offset*this.state.perPage), (this.state.offset*this.state.perPage)+ this.state.perPage)},()=>{
-                this.setState({pageCount:this.state.todos.length/this.state.perPage});
-            }) ;
-          }else{
-              let temp=this.state.todos.filter(x => x.completed===this.state.seletedType);
-            this.setState({
-                    filteredTodos:temp.slice((this.state.offset*this.state.perPage),
-                     (this.state.offset*this.state.perPage)+ this.state.perPage)},()=>{
-                this.setState({pageCount:temp.length/this.state.perPage});
-          });
+    //sub function : Categorize items
+    updateList=()=>{
+        if(this.state.seletedType==="all"){
+        this.setState({filteredTodos:this.state.todos.slice((this.state.offset*this.state.perPage), (this.state.offset*this.state.perPage)+ this.state.perPage)},()=>{
+            this.setState({pageCount:this.state.todos.length/this.state.perPage});
+        }) ;
+        }else{
+            let temp=this.state.todos.filter(x => x.completed===this.state.seletedType);
+        this.setState({
+                filteredTodos:temp.slice((this.state.offset*this.state.perPage),
+                    (this.state.offset*this.state.perPage)+ this.state.perPage)},()=>{
+            this.setState({pageCount:temp.length/this.state.perPage});
+        });
        
       }
     }
+    // function : mark todo as completed
+    handleRowClick = (e,completed) => {
+        e.preventDefault();
+        this.setState(state => ({
+            todos: state.todos.map(todo => {
+              if (todo.id === completed.id) {
+                    return {
+                        ...todo,
+                        completed:true
+                  };
+             } else {
+                 return todo;
+             }
+         })
+     }));
 
+     this.updateList();
+    };
+    
     render(){
          
         return(
@@ -133,7 +158,7 @@ export default class Home extends Component {
                 <div>
                 <table>
                     <thead>
-                        <tr>
+                        <tr >
                             <th>Id</th>
                             <th>Title</th>
                             <th>
@@ -150,7 +175,7 @@ export default class Home extends Component {
                     <tbody>
                        {this.state.filteredTodos.map(x => {
                          return (
-                              <tr key={x.id}>
+                              <tr key={x.id}  onClick={ e => this.handleRowClick(e, x) }>
                               <td>{x.id}</td>
                               <td>{x.title}</td>
                               <td >{x.completed ? 'Done': 'Active'}</td>
